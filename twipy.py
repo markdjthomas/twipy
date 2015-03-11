6fbb080a14516ade854b523839c218b87fe2bc8d
markdjthomas <markdjthomas@gmail.com>
2015-03-11 13:16:19 -0300

    Character parser
    Included a character parser so “<-“ no longer appears as “&lt;-“.

--- twipy.py
+++ twipy.py
@@ -1,14 +1,14 @@
 import re
 import tweepy
+import HTMLParser
 
 def twipy():
     AUTH = tweepy.OAuthHandler('consumer_key', 'consumer_secret')
     API = tweepy.API(AUTH)
     timeline = API.user_timeline('RLangTip')[0]
     tweet = str(timeline.text)
-    tweet = re.sub('(#.+)', '', tweet)  # comment out if you want hashtags
-    tweet = re.sub('(http://.+)', '', tweet)  # comment out if you want urls
-    print '@RLangTip: ' + tweet
+    parser = HTMLParser.HTMLParser()
+    print parser.unescape('@RLangTip: ' + tweet)
     
 if __name__ == "__main__": 
     twipy()
